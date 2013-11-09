5960e644fcf9d97e61c5153935d187d92798932b
nmrugg <nmrugg@gmail.com>
2013-11-09 22:09:00 +0800

    Don't pollute the global scope if we don't have to.

--- src/lzma_worker.js
+++ src/lzma_worker.js
@@ -3904,14 +3904,18 @@ var LZMA = (function () {
 		}
 	}());
 	
-	/// Create the global onmessage function.
-	onmessage = function (e) {
-		if (e.data.action === action_compress) {
-			LZMA.compress(e.data.data, e.data.mode, e.data.callback_num);
-		} else {
-			LZMA.decompress(e.data.data, e.data.callback_num);
-		}
-	}
+	if (typeof Worker !== "undefined") {
+	   (function create_onmessage() {
+            /// Create the global onmessage function.
+            onmessage = function (e) {
+                if (e.data.action === action_compress) {
+                    LZMA.compress(e.data.data, e.data.mode, e.data.callback_num);
+                } else {
+                    LZMA.decompress(e.data.data, e.data.callback_num);
+                }
+            }
+        }());
+    }
 		
 	return {
 		compress:   compress,
