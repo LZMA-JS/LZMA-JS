a6a63cf2d4df985893fd04e8cafd78c9e38d6d51
nmrugg <nmrugg@gmail.com>
2015-05-12 16:33:08 +0800

    Syncing gh-pages

--- demos/simple_node_demo.js
+++ demos/simple_node_demo.js
@@ -1,15 +1,15 @@
 /// Usage: $ node simple_node_demo.js [text [mode]]
 
 ///NOTE: You can install LZMA-JS via npm with this command: $ npm install lzma
-///      Then you can load the code with the following code: var my_lzma = require("lzma").LZMA();
-var my_lzma = require("../src/lzma.js").LZMA(),
+///      Then you can load the code with the following code: var my_lzma = require("lzma");
+var my_lzma = require("../index.js"),
     compress_me = process.argv[2] || "Hello, world.",
     compression_mode = process.argv[3] || 1;
 
 /// First, let's compress it.
 my_lzma.compress(compress_me, compression_mode, function (result) {
-    console.log("Compressed: " + result);
-    
+    ///NOTE: LZMA-JS returns a regular JavaScript array. You can turn it into a buffer like so.
+    console.log("Compressed: ", new Buffer(result));
     /// Now, let's try to decompress it to make sure it works both ways.
     my_lzma.decompress(result, function (result) {
         console.log("Decompressed: " + result);
