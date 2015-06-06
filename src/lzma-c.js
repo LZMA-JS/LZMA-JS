3d542ceea701be9eb5d846ad007a88102ba80d1b
nmrugg <nmrugg@gmail.com>
2015-06-06 11:48:37 +0800

    Minified:
    lzma_worker.js
    Original size: 113747 bytes (111.1 KB)
    Minified size: 24558 bytes (24.0 KB)
    Compression:   4.6318 x smaller
    Gzipped size:  9534 bytes (9.3 KB)
    Gzipped ratio: 11.9307 x smaller
    
    lzma-c.js
    Original size: 87196 bytes (85.2 KB)
    Minified size: 18915 bytes (18.5 KB)
    Compression:   4.6099 x smaller
    Gzipped size:  7596 bytes (7.4 KB)
    Gzipped ratio: 11.4792 x smaller
    
    lzma-d.js
    Original size: 32601 bytes (31.8 KB)
    Minified size: 7147 bytes (7.0 KB)
    Compression:   4.5615 x smaller
    Gzipped size:  3077 bytes (3.0 KB)
    Gzipped ratio: 10.5951 x smaller
    
    lzma.js
    Original size: 6182 bytes (6.0 KB)
    Minified size: 1597 bytes (1.6 KB)
    Compression:   3.8710 x smaller
    Gzipped size:  735 bytes (0.72 KB)
    Gzipped ratio: 8.4109 x smaller

--- src/lzma-c.js
+++ src/lzma-c.js
@@ -1355,8 +1355,7 @@ var LZMA = (function () {
             }
             if (newLen > numAvailableBytes) {
                 newLen = numAvailableBytes;
-                for (numDistancePairs = 0; newLen > this$static._matchDistances[numDistancePairs]; numDistancePairs += 2) {
-                }
+                for (numDistancePairs = 0; newLen > this$static._matchDistances[numDistancePairs]; numDistancePairs += 2) {}
                 this$static._matchDistances[numDistancePairs] = newLen;
                 numDistancePairs += 2;
             }
@@ -1854,7 +1853,7 @@ var LZMA = (function () {
     }());
     
     function $Encode_3(this$static, probs, index, symbol) {
-        var newBound, prob = prob = probs[index];
+        var newBound, prob = probs[index];
         newBound = (this$static.Range >>> 11) * prob;
         if (symbol == 0) {
             this$static.Range = newBound;
