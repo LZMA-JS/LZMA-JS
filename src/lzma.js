LZMA-JS/LZMA-JS

commit b1b851361b08f46ae694f0a21b69a8cea32e2e5c

Allow direct Uint8Array output
Add a `is_utf8` parameter to `decompress()`, which defaults
to `true` (i.e. current behaviour). When set to `false`,
makes the decompressor always return a raw `Uint8Array` instance.

src/lzma.js
@@ -50,14 +50,14 @@ if (typeof Worker === "undefined" || (typeof location !== "undefined" && locatio
                             }, 50);
                         }
                     },
-                    decompress: function decompress(byte_arr, on_finish, on_progress) {
+                    decompress: function decompress(byte_arr, is_utf8, on_finish, on_progress) {
                         if (global_var.LZMA_WORKER) {
-                            global_var.LZMA_WORKER.decompress(byte_arr, on_finish, on_progress);
+                            global_var.LZMA_WORKER.decompress(byte_arr, is_utf8, on_finish, on_progress);
                         } else {
                             /// Wait
                             setTimeout(function ()
                             {
-                                fake_lzma.decompress(byte_arr, on_finish, on_progress);
+                                fake_lzma.decompress(byte_arr, is_utf8, on_finish, on_progress);
                             }, 50);
                         }
                     },
@@ -141,8 +141,8 @@ if (typeof Worker === "undefined" || (typeof location !== "undefined" && locatio
                 compress: function compress(mixed, mode, on_finish, on_progress) {
                     send_to_worker(action_compress, mixed, mode, on_finish, on_progress);
                 },
-                decompress: function decompress(byte_arr, on_finish, on_progress) {
-                    send_to_worker(action_decompress, byte_arr, false, on_finish, on_progress);
+                decompress: function decompress(byte_arr, is_utf8, on_finish, on_progress) {
+                    send_to_worker(action_decompress, byte_arr, is_utf8, on_finish, on_progress);
                 },
                 worker: function worker() {
                     return lzma_worker;

src/lzma_worker.js
@@ -2391,13 +2391,19 @@ var LZMA = (function () {
     }
     /** ce */
     /** ds */
-    function decompress(byte_arr, on_finish, on_progress) {
+    function decompress(byte_arr, is_utf8, on_finish, on_progress) {
         var this$static = {},
             percent,
             cbn, /// A callback number should be supplied instead of on_finish() if we are using Web Workers.
             has_progress,
             len,
             sync = typeof on_finish == "undefined" && typeof on_progress == "undefined";
+        
+        if (typeof is_utf8 !== "boolean") {
+            on_progress = on_finish;
+            on_finish = is_utf8;
+            is_utf8 = true;
+        }
 
         if (typeof on_finish != "function") {
             cbn = on_finish;
@@ -2426,7 +2432,9 @@ var LZMA = (function () {
         if (sync) {
             this$static.d = $LZMAByteArrayDecompressor({}, byte_arr);
             while ($processChunk(this$static.d.chunker));
-            return decode($toByteArray(this$static.d.output));
+            var bytes = $toByteArray(this$static.d.output);
+            
+            return is_utf8 ? decode(bytes) : bytes;
         }
         
         try {
@@ -2461,7 +2469,8 @@ var LZMA = (function () {
                 
                 on_progress(1);
                 
-                res = decode($toByteArray(this$static.d.output));
+                var bytes = $toByteArray(this$static.d.output);
+                res = is_utf8 ? decode(bytes) : bytes;
                 
                 /// delay so we don’t catch errors from the on_finish handler
                 wait(on_finish.bind(null, res), 0);
@@ -2511,7 +2520,8 @@ var LZMA = (function () {
                 if (e && e.data) {
                     /** xs */
                     if (e.data.action == action_decompress) {
-                        LZMA.decompress(e.data.data, e.data.cbn);
+                        // use data.mode as the is_utf8 argument
+                        LZMA.decompress(e.data.data, e.data.mode, e.data.cbn);
                     } else if (e.data.action == action_compress) {
                         LZMA.compress(e.data.data, e.data.mode, e.data.cbn);
                     }

commit b41ef1fdad1b85e9c1935655afea8b02d10233dd

Fix issue with advanced demo/decompress without `is_utf8` set
Fix bug introduced in b1b85136 that would potentitally break
WebWorker support.

src/lzma.js
@@ -118,6 +118,12 @@ if (typeof Worker === "undefined" || (typeof location !== "undefined" && locatio
         return (function () {
             
             function send_to_worker(action, data, mode, on_finish, on_progress) {
+                if (typeof mode === 'function') {
+                    on_progress = on_finish;
+                    on_finish = mode;
+                    mode = true; // default to assuming utf8 output
+                }
+                
                 var cbn;
                 
                 do {