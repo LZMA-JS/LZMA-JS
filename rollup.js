07ceb8fd6cf4ff70a0bc0f369fd2a5fa1a2f6632
Kaito Udagawa <umireon@gmail.com>
2020-02-14 10:58:54 +0900

    Update minify.js to use rollup

--- rollup.js
+++ rollup.js
@@ -3,49 +3,51 @@ const terser = require("rollup-plugin-terser").terser;
 const umdFooter = "var LZMA_WORKER = this.LZMA;";
 const umdModuleName = "LZMA";
 
-["lzma", "lzma_worker", "lzma-c", "lzma-d"].forEach(function (name) {
-    rollup.rollup({
-        input: `src/es/${name}.js`,
-    }).then(function (bundle) {
-        bundle.write({
-            format: "umd",
-            file: `src/${name}.js`,
-            footer: umdFooter,
-            name: umdModuleName,
-        })
-    }).catch(console.error)
-
-    rollup.rollup({
-        input: `src/es/${name}.js`,
-        plugins: [
-            terser({
-                compress: {
-                    unsafe: true,
-                    unsafe_comps: true,
-                    pure_getters: true
-                },
-                mangle: {
-                    toplevel: true,
-                    reserved: ["LZMA", "LZMA_WORKER", "compress", "decompress"],
-                    properties: {
-                        regex: /^(?!LZMA|(de)?compress|data|mode|cbn)/
-                    }
-                }
+module.exports.minify = function minify() {
+    ["lzma", "lzma_worker", "lzma-c", "lzma-d"].forEach(function (name) {
+        rollup.rollup({
+            input: `src/es/${name}.js`,
+        }).then(function (bundle) {
+            bundle.write({
+                format: "umd",
+                file: `src/${name}.js`,
+                footer: umdFooter,
+                name: umdModuleName,
             })
-        ]
-    }).then(function (bundle) {
-        bundle.write({
-            format: "umd",
-            file: `src/${name}-min.js`,
-            footer: umdFooter,
-            name: umdModuleName,
-            sourceMap: true
-        })
+        }).catch(console.error)
 
-        bundle.write({
-            format: "esm",
-            file: `src/${name}-min.mjs`,
-            sourceMap: true
-        })
-    }).catch(console.error)
-})
+        rollup.rollup({
+            input: `src/es/${name}.js`,
+            plugins: [
+                terser({
+                    compress: {
+                        unsafe: true,
+                        unsafe_comps: true,
+                        pure_getters: true
+                    },
+                    mangle: {
+                        toplevel: true,
+                        reserved: ["LZMA", "LZMA_WORKER", "compress", "decompress"],
+                        properties: {
+                            regex: /^(?!LZMA|(de)?compress|data|mode|cbn)/
+                        }
+                    }
+                })
+            ]
+        }).then(function (bundle) {
+            bundle.write({
+                format: "umd",
+                file: `src/${name}-min.js`,
+                footer: umdFooter,
+                name: umdModuleName,
+                sourceMap: true
+            })
+
+            bundle.write({
+                format: "esm",
+                file: `src/${name}-min.mjs`,
+                sourceMap: true
+            })
+        }).catch(console.error)
+    })
+}
